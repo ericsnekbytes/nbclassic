f4d7626b7b783f1e7ff46b44212d2103ec5e5cd4
RRosio <rosio_reyes@yahoo.com>
2022-10-14 12:28:18 -0700

    commented regex

--- grab_dependencies.py
+++ grab_dependencies.py
@@ -5,7 +5,14 @@ def run():
     with open("nbclassic.egg-info/requires.txt", "r", encoding="utf8") as fhandle:
         text = fhandle.read()
 
-    match = re.search(r'([^\[]+)(?:[^\[]+\[(?!t)[^\[]+)*\[test\]([^\[]+)', text)
+    match = re.search(
+        ("""([^\[]+)              # Grab everything before first bracket
+         (?:[^\[]+\[(?!t)[^\[]+)* # Match and Ignore non [test] sections
+         \[test\]                 # Match and ignore the test header
+         ([^\[]+)                 # Match everythin inside the test section"""),
+        text,
+        flags=re.VERBOSE
+    )
 
     with open("test_requirements.txt", "w", encoding="utf8") as fhandle:
         fhandle.write(match.group(1).strip()+'\n')
