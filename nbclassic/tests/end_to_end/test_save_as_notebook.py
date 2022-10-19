24f84c05877960a6eb9af7caa65d5b0e30be4990
RRosio <rosio_reyes@yahoo.com>
2022-10-19 11:54:02 -0700

    update saveas test

--- nbclassic/tests/end_to_end/test_save_as_notebook.py
+++ nbclassic/tests/end_to_end/test_save_as_notebook.py
@@ -36,7 +36,10 @@ def test_save_notebook_as(notebook_frontend):
     notebook_frontend.insert_text('new_notebook.ipynb', page=EDITOR_PAGE)
     notebook_frontend.try_click_selector('//html//body//div[8]//div//div//div[3]//button[2]', page=EDITOR_PAGE)
     
-    locator_element.expect_not_to_be_visible()
+    # locator_element.expect_not_to_be_visible()
+    notebook_frontend.wait_for_condition(
+        lambda: get_notebook_name(notebook_frontend) == "new_notebook.ipynb", timeout=120, period=5
+    )
 
-    assert get_notebook_name(notebook_frontend) == "new_notebook.ipynb"
+    # assert get_notebook_name(notebook_frontend) == "new_notebook.ipynb"
     assert "new_notebook.ipynb" in notebook_frontend.get_page_url(page=EDITOR_PAGE)
