be376d66e2d0228663d13db94239171ba8605753
Eric Gentry <ericsnekbytes@gmail.com>
2023-03-10 10:16:01 -0500

    First draft complete test rewrite

--- nbclassic/tests/end_to_end/test_dashboard_nav.py
+++ nbclassic/tests/end_to_end/test_dashboard_nav.py
@@ -15,58 +15,28 @@ def url_in_tree(nb, url=None):
     return True if tree_url in url else False
 
 
-def get_list_items(nb):
-    """
-    Gets list items from a directory listing page
-    """
+def navigate_to_link(nb, item_text):
+    notebook_frontend = nb
 
-    nb.wait_for_selector('#notebook_list .item_link', page=TREE_PAGE)
-    link_items = nb.locate_all('#notebook_list .item_link', page=TREE_PAGE)
-
-    results = []
-    for item in link_items:
-        print(f'             Found link item:')
-        print(f'                 {item._bool}')
-        print(f'                 {item._element}')
-        if item and item.is_visible():
-            inner_text = item.get_inner_text()
-            print(f'                 text: "{inner_text}"')
-            if inner_text != '..':
-                print(f'                 adding link to results')
-                results.append({
-                    'link': item.get_attribute('href'),
-                    'label': inner_text,
-                    'element': item,
-                })
-        else:
-            print(f'                 invalid element!')
-
-    return results
-    # return [{
-    #     'link': a.get_attribute('href'),
-    #     'label': a.get_inner_text(),
-    #     'element': a,
-    # } for a in link_items if a and a.get_inner_text() != '..']
-
-
-def test_navigation(notebook_frontend):
-    print('[Test] [test_dashboard_nav] Start! y2')
-
-    # os.makedirs(pjoin(nbdir, 'sub ∂ir1', 'sub ∂ir 1a'))
-    # os.makedirs(pjoin(nbdir, 'sub ∂ir2', 'sub ∂ir 1b'))
-    SUBDIR1 = 'sub ∂ir1'
-    SUBDIR2 = 'sub ∂ir2'
-    SUBDIR1A = 'sub ∂ir1a'
-    SUBDIR1B = 'sub ∂ir1b'
-
-    start_url = notebook_frontend.get_page_url(page=TREE_PAGE)
-    links = notebook_frontend.locate_all('#notebook_list .item_link', TREE_PAGE)
-    target_element = [item for item in links if item.get_inner_text().strip() == SUBDIR1][0]
+    print(f'[Test]   Now navigating to "{item_text}"')
+    def get_matches():
+        tree_page_items = [item for item in notebook_frontend.locate_all('#notebook_list .item_link', TREE_PAGE)]
+        print(f'     Attempt to find matches...')
+        for elem in tree_page_items:
+            print(f'       {elem._element}')
+            if elem.is_visible():
+                print(f'         {elem.get_inner_text()}')
+        return [elem for elem in tree_page_items if elem.is_visible() and elem.get_inner_text().strip() == item_text]
+    matches = notebook_frontend.wait_for_condition(
+        lambda: get_matches(),
+        period=3
+    )
+    target_element = matches[0]
     target_link = target_element.get_attribute("href")
     target_element.click()
     notebook_frontend.wait_for_condition(
         lambda: not notebook_frontend.locate(
-            f'#notebook_list .item_link >> text="{SUBDIR1}"',
+            f'#notebook_list .item_link >> text="{item_text}"',
             page=EDITOR_PAGE).is_visible()
     )
     print(f'[Test]     Check URL in tree')
@@ -84,10 +54,76 @@ def test_navigation(notebook_frontend):
         period=5
     )
     print(f'[Test]     Passed!')
+    return notebook_frontend.get_page_url(page=TREE_PAGE)
 
+
+def test_navigation(notebook_frontend):
+    print('[Test] [test_dashboard_nav] Start! y2')
+
+    # NOTE the paths here are determined by X
+
+    # os.makedirs(pjoin(nbdir, 'sub ∂ir1', 'sub ∂ir 1a'))
+    # os.makedirs(pjoin(nbdir, 'sub ∂ir2', 'sub ∂ir 1b'))
+    SUBDIR1 = 'sub ∂ir1'
+    SUBDIR2 = 'sub ∂ir2'
+    SUBDIR1A = 'sub ∂ir 1a'
+    SUBDIR1B = 'sub ∂ir 1b'
+
+    # Start at the root (tree) URL
+    print(f'[Test] Start at tree (root) URL')
+    tree_url = notebook_frontend.get_page_url(page=TREE_PAGE)
+    print(f'[Test]   URL is now at "{tree_url}"')
+
+    # Test navigation to first folder in root
+    print(f'[Test] Navigate to subdir1')
+    subdir1_url = navigate_to_link(notebook_frontend, SUBDIR1)
+    print(f'[Test]   URL is now at "{subdir1_url}"')
+
+    # Test navigation to first subfolder
+    print(f'[Test] Navigate to subdir1a')
+    subdir1a_url = navigate_to_link(notebook_frontend, SUBDIR1A)
+    print(f'[Test]   URL is now at "{subdir1a_url}"')
     notebook_frontend.go_back(page=TREE_PAGE)
     notebook_frontend.wait_for_condition(
-        lambda: notebook_frontend.get_page_url(page=TREE_PAGE) == start_url,
+        lambda: notebook_frontend.get_page_url(page=TREE_PAGE) == subdir1_url,
+        timeout=300,
+        period=5
+    )
+    notebook_frontend.go_back(page=TREE_PAGE)
+    notebook_frontend.wait_for_condition(
+        lambda: notebook_frontend.get_page_url(page=TREE_PAGE) == tree_url,
+        timeout=300,
+        period=5
+    )
+    notebook_frontend.wait_for_condition(
+        lambda: not notebook_frontend.locate(
+            f'#notebook_list .item_link >> text="{SUBDIR1A}"',
+            page=EDITOR_PAGE).is_visible()
+    )
+
+    # Test navigation to second folder from root
+    print(f'[Test] Navigate to subdir2')
+    subdir2_url = navigate_to_link(notebook_frontend, SUBDIR2)
+    print(f'[Test]   URL is now at "{subdir2_url}"')
+    notebook_frontend.wait_for_condition(
+        lambda: not notebook_frontend.locate(
+            f'#notebook_list .item_link >> text="{SUBDIR2}"',
+            page=EDITOR_PAGE).is_visible()
+    )
+
+    # Test navigation to second subfolder
+    print(f'[Test] Navigate to subdir1b')
+    subdir1b_url = navigate_to_link(notebook_frontend, SUBDIR1B)
+    print(f'[Test]   URL is now at "{subdir1b_url}"')
+    notebook_frontend.go_back(page=TREE_PAGE)
+    notebook_frontend.wait_for_condition(
+        lambda: notebook_frontend.get_page_url(page=TREE_PAGE) == subdir2_url,
+        timeout=300,
+        period=5
+    )
+    notebook_frontend.go_back(page=TREE_PAGE)
+    notebook_frontend.wait_for_condition(
+        lambda: notebook_frontend.get_page_url(page=TREE_PAGE) == tree_url,
         timeout=300,
         period=5
     )
